a1ac96ad98a33fed4e7267a51d386ad7a03d2c86
Grant Collins <grantcollins03@gmail.com>
2022-10-10 19:05:42 -0400

    Created operate() to call basic math functions

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, x, y) {
+    switch (operator) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            return divide(x, y);
+        default:
+            return "Something ain't right.";
+    }
+}
