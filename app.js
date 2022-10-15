446914fae2735e9cf8829c7e36d93b2e33415436
Grant Collins <grantcollins03@gmail.com>
2022-10-15 18:37:52 -0400

    Added number character limit to each number and decimal btns

--- app.js
+++ app.js
@@ -41,53 +41,93 @@ let operator = '';
 //Number buttons to populate display
 
 document.getElementById('0-btn').addEventListener('click', function(){
-    displayValue += '0';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '0';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('1-btn').addEventListener('click', function(){
-    displayValue += '1';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '1';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('2-btn').addEventListener('click', function(){
-    displayValue += '2';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '2';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('3-btn').addEventListener('click', function(){
-    displayValue += '3';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '3';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('4-btn').addEventListener('click', function(){
-    displayValue += '4';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '4';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('5-btn').addEventListener('click', function(){
-    displayValue += '5';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '5';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('6-btn').addEventListener('click', function(){
-    displayValue += '6';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '6';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('7-btn').addEventListener('click', function(){
-    displayValue += '7';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '7';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('8-btn').addEventListener('click', function(){
-    displayValue += '8';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '8';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 document.getElementById('9-btn').addEventListener('click', function(){
-    displayValue += '9';
-    document.getElementById('display').textContent = displayValue;
+    if (displayValue.length < 12) {
+        displayValue += '9';
+        document.getElementById('display').textContent = displayValue;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 //Decimal button, which can only be clicked once before an operator is clicked
@@ -95,9 +135,13 @@ document.getElementById('9-btn').addEventListener('click', function(){
 let decimalBtn = document.getElementById('decimal-btn');
 
 decimalBtn.addEventListener('click', function(){
-    displayValue += '.';
-    document.getElementById('display').textContent = displayValue;
-    decimalBtn.disabled = true;
+    if (displayValue.length < 12) {
+        displayValue += '.';
+        document.getElementById('display').textContent = displayValue;
+        decimalBtn.disabled = true;
+    } else {
+        alert("You've reached the number character limit.")
+    }
 });
 
 //Operator buttons to assign firstNum a value and reset the display value
@@ -105,6 +149,7 @@ document.getElementById('plus-btn').addEventListener('click', function(){
     if (operator !== '') {
         secondNum = +displayValue;
         displayValue = operate(operator, firstNum, secondNum);
+
         document.getElementById('display').textContent = displayValue;
     }
     operator = '+';   
@@ -177,7 +222,7 @@ document.getElementById('backspace-btn').addEventListener('click', function(){
 document.getElementById('equals-btn').addEventListener('click', function(){
     if (operator !== '') {
         secondNum = +displayValue;
-        if (secondNum === 0) {
+        if ((operator === '/') && (secondNum === 0)) {
             alert("You can't divide by zero!");
             allClear();
         } else {
@@ -187,6 +232,20 @@ document.getElementById('equals-btn').addEventListener('click', function(){
         }
     }
 });
+
+// function round(answer) {
+//     let decimalIndex = answer.toString().indexOf('.');
+//     if ((decimalIndex > -1) && (answer[decimalIndex + 4] !== undefined)) {
+//         return answer.parseFloat().toFixed(3);
+//     }
+// }
+
+
+function limitDisplay(displayValue) {
+    if (displayValue.length > 12) {
+        return displayValue.slice(0, 12);
+    }
+}
 
 //displayValue cannot be more than 12 characters long
 
